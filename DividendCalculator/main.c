7422ad4e113a57cb698c4e519941827d70902777
Kevinw14 <kevinlw14@gmail.com>
2021-05-10 11:58:29 -0400

    Initial Commit

---
+++ DividendCalculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DividendCalculator
+//
+//  Created by Kevin Wood on 5/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
